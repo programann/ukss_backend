92bf9fee4c28e0f0635f49bc8600b25a969dd47c
clarence <mclarence311@gmail.com>
2025-01-09 17:43:57 +0300

    add admin and models

---
+++ server/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from config import Config
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    CORS(app)
+
+    from .routes import admin, events
+    app.register_blueprint(admin.bp, url_prefix="/admin")
+    app.register_blueprint(events.bp, url_prefix="/events")
+
+    return app
